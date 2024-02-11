38001cf707d8ec6e662cc9ffde743fa13248af5a
JohnGabz <159271723+JohnGabz@users.noreply.github.com>
2024-02-11 18:54:09 +0800

    Update Person.java

--- Project 0/Person.java
+++ Project 0/Person.java
@@ -59,7 +59,7 @@ public class Person implements Comparable<Person>
      */
     public String getPronoun()
     {
-        return this.getSex().equals("Male") ? "His" : "Her";
+        return this.getSex().equals("M") ? "His" : "Her";
     }
 
     public String getOccupation()
